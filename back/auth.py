ebeec7e08f51b8b20ca41b7e657b638572648721
Benjamin Cruz <A01747811@tec.mx>
2025-06-04 00:17:25 -0600

    feat(auth.py)
    Agrega funciones de autenticación y manejo de tokens JWT

--- back/auth.py
+++ back/auth.py
@@ -61,9 +61,9 @@ async def get_current_user(
         raise credentials_exception
     return user
 
-async def get_current_active_user(
-    current_user: models.User = Depends(get_current_user),
-) -> models.User:
-    if not current_user.is_active:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
+# async def get_current_active_user(
+#     current_user: models.User = Depends(get_current_user),
+# ) -> models.User:
+#     if not current_user.is_active:
+#         raise HTTPException(status_code=400, detail="Inactive user")
+#     return current_user
