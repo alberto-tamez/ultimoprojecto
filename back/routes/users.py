b7f336c2786430422ca5523d58ac251546084047
Bernardo0173 <limonbernardo99@gmail.com>
2025-06-05 13:30:27 -0600

    Actualización de users.py para reflejar cambios en el modelo de usuarios

--- back/routes/users.py
+++ back/routes/users.py
@@ -5,13 +5,13 @@ from database import get_db
 
 router = APIRouter()
 
-@router.post("/users/", response_model=schemas.UserInDB)
+@router.post("/users/", response_model=schemas.UserResponse)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
 
-@router.get("/users/me/", response_model=schemas.UserInDB)
+@router.get("/users/me/", response_model=schemas.UserResponse)
 async def read_users_me(current_user: models.User = Depends(auth.get_current_user)):
     return current_user
